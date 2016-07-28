8c323aaeca098b07c0ab12ae28407b8a0e6ef053
shaozhenxing <15330077113@163.com>
2016-07-28 12:08:42 +0800

    03

---
+++ src/App-01.vue
@@ -0,0 +1,19 @@
+<template>
+  <h6>{{ msg }}</h6>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '孙子'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h6 {
+  color: green;
+}
+</style>
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <app-01></app-01>
+</template>
+
+<script>
+import app01 from './App-01.vue'
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+  components:{app01}
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
